a96feb43cf75a75ed359bada6723b21cab1b961f
Elea Poirot <contact.elea.poirot@gmail.com>
2026-05-19 16:25:44 +0200

    build : ajout lib json

--- TD_train/draw_scene.hpp
+++ TD_train/draw_scene.hpp
@@ -4,6 +4,7 @@
 #include "glbasimac/glbi_set_of_points.hpp"
 #include "glbasimac/glbi_convex_2D_shape.hpp"
 #include "tools/basic_mesh.hpp"
+#include "nlohmann/json.hpp"
 
 using namespace glbasimac;
 
