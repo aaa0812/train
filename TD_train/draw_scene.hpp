ac021e225381acb5d0b2a30e80e5a3e532442e35
Elea Poirot <contact.elea.poirot@gmail.com>
2026-06-03 16:47:09 +0200

    refactor : séparation de ce qui concerne le chemin de fer dans un autre fichier pour une meilleure lisibilité

--- TD_train/draw_scene.hpp
+++ TD_train/draw_scene.hpp
@@ -4,6 +4,7 @@
 #include "custom_shapes.hpp"
 #include "scene_items.hpp"
 #include "train_items.hpp"
+#include "railroad.hpp"
 
 /* Camera parameters and functions */
 static const float Z_NEAR{0.1f};
@@ -14,27 +15,9 @@ extern float dist_zoom;   // Distance between origin and viewpoint
 extern bool flag_anim_rot_scale;
 extern bool flag_anim_rot_arm;
 
-enum Orientation
-{
-    BottomRight,
-    BottomLeft,
-    TopRight,
-    TopLeft,
-};
-
 void initScene(GridConfig const &gridConfig);
 
 void drawGround(bool displayGrid = false);
-
-void drawOneRail();
-
-void drawOneCurvedRail(int maxIteration = 10, float position = 3);
-
-void drawBallast();
-
-void drawCompleteRail(int posX, int posY, float rotation);
-
-void drawCompleteCurvedRail(int posX, int posY, float rotation, STP3D::Vector3D offset = {0.f, 0.f, 0.f});
 
 void drawFrame();
 
@@ -42,12 +25,8 @@ void drawSphere(double time_ellapsed);
 
 void rotateSphere(double time_ellapsed, float radius, STP3D::Vector3D origin_point = {0.0, 0.0, 0.0});
 
-void drawRailRoad();
-
 void drawLantern(int posX, int posY, float scale = 1.f);
 
 void drawCompleteLantern(int posX, int posY);
 
 void drawScene(double time_ellapsed, bool displayGrid = false);
-
-Orientation defineCurveDir(Position prev, Position current, Position next);
